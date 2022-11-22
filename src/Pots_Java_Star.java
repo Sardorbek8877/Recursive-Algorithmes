aefdf7b9bdea7d755d2360c23aab296e69d9af77
Sardorbek8877 <sardorbeksafarov8877@gmail.com>
2022-11-22 13:16:54 +0100

    onloading containers

--- src/Pots_Java_Star.java
+++ src/Pots_Java_Star.java
@@ -14,4 +14,25 @@ public class Pots_Java_Star {
     private static int computeInterimPosition( int start, int target ){
         return 3 - start - target;
     }
+
+    private static boolean existsBalanceUpTo(int[] containers, int limit, int divergence, int unitsLoaded){
+        if (unitsLoaded >= containers.length){
+            return true;
+        }
+
+        int nextDivergence = divergence + containers[unitsLoaded];
+        if (Math.abs( nextDivergence ) <= limit){
+            boolean loadedOnPortSide = existsBalanceUpTo(containers, limit, nextDivergence, unitsLoaded+1);
+            if ( loadedOnPortSide ){
+                return true;
+            }
+        }
+        nextDivergence = divergence - containers[unitsLoaded];
+        if (Math.abs( nextDivergence ) <= limit){
+            return existsBalanceUpTo( containers, limit, nextDivergence, unitsLoaded+1);
+        }
+        else {
+            return false;
+        }
+    }
 }
